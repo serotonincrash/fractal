6e437853d90565a9c522979f5c92eb09b46bb3e5
serotonincrash <derpydm@gmail.com>
2022-04-08 15:42:20 +0800

    initial commit, base SwiftUI project

---
+++ Fractal/Shared/FractalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalApp.swift
+//  Shared
+//
+//  Created by sen on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FractalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
